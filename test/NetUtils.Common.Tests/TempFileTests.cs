56306f958ba6707f15e04aca5e8e54d2579b32a1
hanabi1224 <harlowmoo@gmail.com>
2019-05-26 04:06:37 +0800

    improve cc

---
+++ test/NetUtils.Common.Tests/TempFileTests.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace NetUtils.Common.Tests
+{
+    [TestFixture]
+    public class TempFileTests
+    {
+        [Test]
+        public void TestTempFile_CreationWithNoData()
+        {
+            string path;
+            using (var tempFile = new TempFile("wav"))
+            {
+                path = tempFile.FullPath;
+                File.Exists(path).Should().BeFalse();
+            }
+
+            File.Exists(path).Should().BeFalse();
+        }
+
+        [Test]
+        public void TestTempFile_CreationWithData()
+        {
+            string path;
+            using (var tempFile = new TempFile(new byte[] { 1, 2, 3 }, "wav"))
+            {
+                path = tempFile.FullPath;
+                File.Exists(path).Should().BeTrue();
+            }
+
+            File.Exists(path).Should().BeFalse();
+        }
+    }
+}
